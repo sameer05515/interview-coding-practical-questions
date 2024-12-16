b8b34dc72c944d7f8bab3f38204c86ff7f01c31d
Riku Ishwariya Chandra Das Gupta <sameer.geeee@gmail.com>
2024-12-16 23:27:12 +0530

    commit

--- javascript/Top-50-Most-Asked-JavaScript-Logical-Interview-Questions/src/questions.js
+++ javascript/Top-50-Most-Asked-JavaScript-Logical-Interview-Questions/src/questions.js
@@ -495,4 +495,25 @@ questions.push(() => {
   //     at node:internal/main/run_main_module:28:49
 });
 
+questions.push(() => {
+  function* myGenerator() {
+    yield 1;
+    yield 2;
+    yield 3;
+  }
+
+  const gen = myGenerator();
+
+  console.log(gen.next());
+  console.log(gen.next());
+  console.log(gen.next());
+  console.log(gen.next());
+
+  // output will be
+  // { value: 1, done: false }
+  // { value: 2, done: false }
+  // { value: 3, done: false }
+  // { value: undefined, done: true }
+});
+
 module.exports = questions;
